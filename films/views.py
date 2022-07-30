7239e5a8386ae9b34899dd649f73b7c97f94e44f
MG <michal.gulczynski93@gmail.com>
2022-07-30 22:24:33 +0200

    Example Css Transition

--- films/views.py
+++ films/views.py
@@ -27,7 +27,7 @@ class RegisterView(FormView):
 def check_username(request):
     username = request.POST.get("username")
     if get_user_model().objects.filter(username=username).exists():
-        return HttpResponse("<div style='color: red;'>'This username already exists</div>")
+        return HttpResponse("<div id='username-error' class='error'>'This username already exists</div>")
     else:
-        return HttpResponse("<div style='color: green;'>This username is avaliable</div>")
+        return HttpResponse("<div id='username-error' class='success'>This username is avaliable</div>")
 
